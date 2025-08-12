d382bc533ac2282747442726c979998b6babea60
sanskriti480 <sanskriti.ten@gmail.com>
2025-08-12 21:43:13 +0530

    Create Exp 8 Addition of matrix.cpp

---
+++ Exp 8 Addition of matrix.cpp
@@ -0,0 +1,107 @@
+//Sanskriti Khairnar
+//PRN-24070123054
+//Batch-A2
+//Experiment 8
+
+#include <iostream>
+#include<cstdio>
+using namespace std;
+
+int main(){
+    //Defining a Multi Dimensional Array
+    int m1,n1;
+    cout<<"Enter number of rows and columns :"<<endl;
+    cin>>m1;
+    cin>>n1;
+    int matrix1[m1][n1];
+    
+    int m2,n2;
+    cout<<"Enter number of rows and columns :"<<endl;
+    cin>>m2;
+    cin>>n2;
+    int matrix2[m2][n2];
+    
+    // Condition Check
+    if(m1 == m2 && n1 == n2){
+        // User Input
+    for(int i = 0; i < m1 ; i++){
+        for(int j = 0; j < n1 ; j++){
+            printf("Enter the element of Matrix1[%d][%d] :\n",i,j);
+            cin>>matrix1[i][j];
+        }
+    }
+    
+    for(int i = 0; i < m2 ; i++){
+        for(int j = 0; j < n2; j++){
+            printf("Enter the element of Matrix2[%d][%d] :\n",i,j);
+            cin>>matrix2[i][j];
+        }
+    }
+    // Printing Matrix
+    cout<<"Matrix 1"<<endl;
+    for(int i = 0; i < m1; i++){
+        for(int j = 0; j<n1; j++){
+            cout<<matrix1[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    
+    cout<<"Matrix 2"<< endl;
+    for(int i = 0; i < m2; i++){
+        for(int j = 0; j<n2; j++){
+            cout<<matrix2[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    int sum[m1][n1];
+    cout<<"Sum"<<endl;
+    for(int i = 0; i < m1; i++){
+        for(int j = 0; j<n1; j++){
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+            cout<<sum[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+}
+    else{
+        cout<<"Invalid";
+        exit(0);
+    }
+    
+    return 0;
+}
+
+//Output
+
+/*Enter number of rows and columns :
+2
+2
+Enter number of rows and columns :
+2
+2
+Enter the element of Matrix1[0][0] :
+1
+Enter the element of Matrix1[0][1] :
+2
+Enter the element of Matrix1[1][0] :
+3
+Enter the element of Matrix1[1][1] :
+4
+Enter the element of Matrix2[0][0] :
+4
+Enter the element of Matrix2[0][1] :
+3
+Enter the element of Matrix2[1][0] :
+2
+Enter the element of Matrix2[1][1] :
+1
+Matrix 1
+1	2	
+3	4	
+Matrix 2
+4	3	
+2	1	
+Sum
+5	5	
+5	5
+*/
